d9d1aa7b90acf93254b7ba5f120de6904ad1e18c
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-08-22 10:38:51 +0530

    find and replac

--- addition.py
+++ addition.py
@@ -4,3 +4,10 @@ grade="A"
 print(name.upper())
 print(name.lower())
 print(name)
+
+#find()
+print(name.find("Har")) #index position
+print(name.find("Har")) 
+
+#replcace
+print(name.replace("Harsh","Rohit"))
