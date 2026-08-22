341ea3f36af9a6692f498168fbfd0802e10ffebe
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-08-22 14:31:48 +0530

    loop

--- addition.py
+++ addition.py
@@ -1,3 +1,11 @@
-#range
-num= range(5)
-print(num)
+#while loop
+counter = 1
+while counter <= 5:
+    print("harsh")
+    counter += 5
+print("end of loop")
+
+i=1
+while i<=5:
+    print(i * "harsh")
+    i+=1
