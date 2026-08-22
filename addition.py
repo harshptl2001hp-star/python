5284931de9c2e9d7e9829ddd5200014cbf3f2ae8
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-08-22 14:21:35 +0530

    range

--- addition.py
+++ addition.py
@@ -1,18 +1,3 @@
-# Calcultor
-
-a = float(input("Enter first number: "))
-b = float(input("Enter the second number: "))
-op = input("Enter operator (+, -, *, /): ")
-
-if op == "+":
-    print(a + b)
-elif op == "-":
-    print(a - b)
-elif op == "*":
-    print(a * b)
-elif op == "/":
-    print(a / b)
-elif op == "**":
-    print(a ** b)
-else:
-    print("Invalid operator")
+#range
+num= range(5)
+print(num)
