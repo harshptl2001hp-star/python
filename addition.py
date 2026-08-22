df88a000df98ab49faf0bb9b3d35313fa1501b7a
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-08-22 13:04:15 +0530

    logical operator

--- addition.py
+++ addition.py
@@ -1,8 +1,6 @@
-# comparison operators
-# >, <, >=, <=, ==, !=
-print(5 > 3)  # True
-print(5 < 3)  # False
-print(5 >= 3)  # True
-print(5 <= 3)  # False
-print(5 == 3)  # False
-print(5 != 3)  # True
+# Logical Operators
+stt1 = 3 > 5
+stt2 = 5 > 12 
+print(stt1 and stt2)  # False
+
+print(not True)  # True
