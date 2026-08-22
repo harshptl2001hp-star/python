e3b69ba74930a3cdf92be782b174b30381f64e62
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-08-22 11:28:33 +0530

    comparison operaator

--- addition.py
+++ addition.py
@@ -1,14 +1,8 @@
-# arithmetic operators
-print(5 + 3)  # addition
-print(5 - 3)  # subtraction
-print(5 * 3)  # multiplication
-print(5 / 3)  # division
-print(5 // 3)  # floor division
-print(5 % 3)  # modulus
-print(5 ** 3)  # exponentiation
-
-# assignment operators
-x = 5
-# x = x + 3 #8
-x += 3  # x = x + 3
-print(x)
+# comparison operators
+# >, <, >=, <=, ==, !=
+print(5 > 3)  # True
+print(5 < 3)  # False
+print(5 >= 3)  # True
+print(5 <= 3)  # False
+print(5 == 3)  # False
+print(5 != 3)  # True
