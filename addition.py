8397f090b84d73a9909685b3761e8a25563223bc
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-09-01 13:21:00 +0530

    nested loop-for pattern

--- addition.py
+++ addition.py
@@ -1,4 +1,6 @@
-#WAP a programm to display the table
-n=int(input("Enter a number: "))
-for i in range(1, 11):
-    print(f"{n} * {i} = {n*i}")
+#nested loop
+c=1
+for i in range(5):
+    for j in range(4):
+        print("Hello",c)
+        c=c+1
