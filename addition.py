f54c162a0c6acd3385bdd5f5e3aeabda519b1af1
harshptl2001hp-star <harshptl2001.hp@gmail.com>
2026-09-15 14:40:08 +0530

    duplicate repeted numbers

--- addition.py
+++ addition.py
@@ -1,3 +1,11 @@
-import numpy as np
-a = [[1, 2, 3, 4, 5, 6], [2,3,4,5,6]]
-array = np.array(a)
+# api->list->remove dup file->new module
+items =[1,2,3,4,4,5,5,6,7,8,8,8,9,10]
+dup = []
+for i in items:
+    count=0
+    for j in items:
+        if i == j:
+            count=count+1
+    if count>1 and i not in dup:
+        dup.append(i) 
+print(dup)
